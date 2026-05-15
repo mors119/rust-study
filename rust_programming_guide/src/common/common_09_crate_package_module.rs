abe739b53b39eae65ecc4a2d937be08514ef5f12
heesk0223@gmail.com <heesk0223@gmail.com>
2026-05-15 14:46:09 +0900

    Fixed Rust-programing-guide

--- rust_programming_guide/src/common/common_09_crate_package_module.rs
+++
@@ -1,7 +0,0 @@
-pub fn run() {
-    // include_str!: 컴파일 시점에 파일 내용을 문자열(&'static str)로 포함
-    let content = include_str!("module_cheat_sheet.md");
-
-    // 파일 내용 전체 출력
-    println!("{content}");
-}
